d61408ad4ff5722b654a6c76dba8a348f3e1c582
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-01 17:06:51 +0100

    dec 1, both parts

---
+++ 1/p1.py
@@ -0,0 +1,13 @@
+import math
+
+
+def fuel_for_mass(m):
+    return int(math.floor(m / 3) - 2)
+
+
+total_fuel = 0
+with open('masses.txt') as masses:
+    for l in masses.readlines():
+        total_fuel += fuel_for_mass(int(l))
+
+print 'total fuel:', total_fuel
