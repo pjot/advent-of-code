3011492ea0a2e8fbf69432dc1f5ee69011545eda
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-05 19:02:24 +0100

    dec 5, refactored memory

--- 5/p2.py
+++ 5/p2.py
@@ -5,88 +5,90 @@ def parse_file(filename):
         program = [int(i) for i in ints]
         return program
 
-def digit(instruction, place):
-    code_map = '{:05d}'.format(instruction)
-    return int(code_map[place])
+def parse_instruction(instruction):
+    full_instruction = '{:05d}'.format(instruction)
+    return (
+        instruction % 100,
+        int(full_instruction[2]),
+        int(full_instruction[1]),
+    )
 
-def read(memory, position, mode):
-    if mode == 1:
-        return memory[position]
-    if mode == 0:
-        return memory[memory[position]]
+class Memory:
+    def __init__(self, memory):
+        self.memory = memory
 
-def write(memory, position, value):
-    memory[memory[position]] = value
+    def read(self, position, mode=1):
+        if mode == 1:
+            return self.memory[position]
+        if mode == 0:
+            return self.memory[self.memory[position]]
+
+    def write(self, position, value):
+        pos = self.read(position)
+        self.memory[pos] = value
 
 def run_program(memory, program_input):
     position = 0
     output = None
 
     while True:
-        code = memory[position]
-        mode_a = digit(code, 2)
-        mode_b = digit(code, 1)
-        op_code = code % 100
+        instruction = memory.read(position)
+        op_code, mode_a, mode_b = parse_instruction(instruction)
 
         if op_code == 99:
             return output
 
         if op_code == 1:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
-            write(memory, position + 3, a + b)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
+            memory.write(position + 3, a + b)
             position += 4
 
         if op_code == 2:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
-            write(memory, position + 3, a * b)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
+            memory.write(position + 3, a * b)
             position += 4
 
         if op_code == 3:
-            write(memory, position + 1, program_input)
+            memory.write(position + 1, program_input)
             position += 2
 
         if op_code == 4:
-            output = read(memory, position + 1, mode_a)
+            output = memory.read(position + 1, mode_a)
             position += 2
 
         if op_code == 5:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
             if a != 0:
                 position = b
             else:
                 position += 3
 
         if op_code == 6:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
             if a == 0:
                 position = b
             else:
                 position += 3
 
         if op_code == 7:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
-            if a < b:
-                write(memory, position + 3, 1)
-            else:
-                write(memory, position + 3, 0)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
+            value = 1 if a < b else 0
+            memory.write(position + 3, value)
             position += 4
 
         if op_code == 8:
-            a = read(memory, position + 1, mode_a)
-            b = read(memory, position + 2, mode_b)
-            if a == b:
-                write(memory, position + 3, 1)
-            else:
-                write(memory, position + 3, 0)
+            a = memory.read(position + 1, mode_a)
+            b = memory.read(position + 2, mode_b)
+            value = 1 if a == b else 0
+            memory.write(position + 3, value)
             position += 4
-
 
     return output
 
 program = parse_file('program.testcode')
-print run_program(program, 5)
+print run_program(Memory(program), 5)
