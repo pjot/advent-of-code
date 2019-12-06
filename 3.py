8a4a169acb95124db5d9c2483244870e0c8e6153
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-06 08:12:52 +0100

    Move 3 to root

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 def coords_for_path(path):
     x, y = 0, 0
-    coords = set()
+    coords = []
     for corner in path:
         direction = corner[0]
         length = int(corner[1:])
@@ -18,8 +18,14 @@ def coords_for_path(path):
         for _ in range(length):
             x += dx
             y += dy
-            coords.add((x, y))
+            coords.append((x, y))
     return coords
+
+def parse_file(file):
+    with open(file) as f:
+        path1 = f.readline().split(',')
+        path2 = f.readline().split(',')
+    return path1, path2
 
 def distance(x, y):
     return abs(x) + abs(y)
@@ -32,16 +38,21 @@ def lowest_distance(coords):
             lowest = d
     return lowest
 
-def parse_file(file):
-    with open(file) as f:
-        path1 = f.readline().split(',')
-        path2 = f.readline().split(',')
-    return path1, path2
+def lowest_step(intersection, coords1, coords2):
+    lowest_step = float('inf')
+    for x, y in intersections:
+        first = coords1.index((x, y))
+        second = coords2.index((x, y))
+        steps = first + second + 2
+        lowest_step = min(steps, lowest_step)
+    return lowest_step
 
 path1, path2 = parse_file('input.path')
+
 coords1 = coords_for_path(path1)
 coords2 = coords_for_path(path2)
 
-intersections = coords1 & coords2
+intersections = set(coords1) & set(coords2)
 
-print lowest_distance(intersections)
+print 'Part 1:', lowest_distance(intersections)
+print 'Part 2:', lowest_step(intersections, coords1, coords2)
