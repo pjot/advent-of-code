0d8a9009db77a2494710185ba4b43a47b67d17ee
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-11 21:53:04 +0100

    dec 08, clojure

--- 08/08.clj
+++ 08/08.clj
@@ -7,7 +7,7 @@
         (string/split-lines)
         (first)) #""))
 
-(defn parse-file [file width height]
+(defn parse-file [file [width height]]
   (->> (first-line file)
        (map #(Integer/parseInt (str %)))
        (partition (* height width))))
@@ -17,10 +17,35 @@
    :count (* (count (filter (partial = 1) layer))
              (count (filter (partial = 2) layer)))})
 
-(def layers (parse-file "image.txt" 25 6))
+(defn zip [a b]
+  (partition 2 (interleave a b)))
+
+(defn next-pixel [[upper lower] & _]
+  (if (= upper 2) lower upper))
+
+(defn merge-layers [upper lower]
+  (map next-pixel (zip upper lower)))
+
+(defn colorize [pixel]
+  (if (= pixel 1) \u2588 " "))
+
+(defn compose-image [layers]
+  (map colorize (reduce merge-layers layers)))
+
+(def size [25 6])
+
+(def layers (parse-file "image.txt" size))
+
 (println "Part 1:"
          (->> layers
               (map count-numbers)
               (sort-by :ones)
               (first)
               (:count)))
+
+(println "Part 2:")
+(println
+ (->> (compose-image layers)
+      (partition (first size))
+      (map string/join)
+      (string/join "\n")))
