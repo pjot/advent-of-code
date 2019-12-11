dc38bf95451d4a9fd429bcfcb7feddeff6f8be62
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-11 22:27:05 +0100

    refactor 4

--- 04/04.clj
+++ 04/04.clj
@@ -5,25 +5,32 @@
   (= (sort s) s))
 
 (defn has-duplicate? [s]
-  (> 6 (count (set s))))
+  (->> (set s)
+       (count)
+       (> 6)))
 
 (defn has-two-of-something? [s]
-  (contains? (set (vals (frequencies s))) 2))
+  (contains?
+   (->> (frequencies s)
+        (vals)
+        (set))
+   2))
 
-(defn digit-split [n] (string/split (str n) #""))
+(defn digit-split [n]
+  (string/split (str n) #""))
 
-(defn is-valid-first [n]
-  (let [per-digit (digit-split n)]
-    (and (sorted-string? per-digit) (has-duplicate? per-digit))))
+(defn is-valid-first [digits]
+  (and (sorted-string? digits)
+       (has-duplicate? digits)))
 
-(defn is-valid-second [n]
-  (let [per-digit (digit-split n)]
-    (and (sorted-string? per-digit) (has-two-of-something? per-digit))))
+(defn is-valid-second [digits]
+  (and (sorted-string? digits)
+       (has-two-of-something? digits)))
 
 (defn valid-in-range [valid from to]
-  (reduce + (map 
-    #(if (valid %) 1 0)
-    (range from to))))
+  (->> (range from to)
+       (filter #(valid (digit-split %)))
+       (count)))
 
 (println "Part 1:" (valid-in-range is-valid-first 240920 789957))
 (println "Part 2:" (valid-in-range is-valid-second 240920 789957))
