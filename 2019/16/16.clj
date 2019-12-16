030c6b3cfd747b2d6cadb7e10b11749c84e7e418
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-16 19:00:30 +0100

    simplify

--- 2019/16/16.clj
+++ 2019/16/16.clj
@@ -3,7 +3,7 @@
 
 (defn parse-file [f]
   (->> (string/split
-          (->> (slurp f)
+        (->> (slurp f)
              (string/split-lines)
              (first)) #"")
        (map #(Integer/parseInt %))
@@ -11,48 +11,45 @@
 
 (defn multipliers-for-phase-implementation [phase]
   (vec
-    (concat
-      (repeat phase 0)
-      (repeat phase 1)
-      (repeat phase 0)
-      (repeat phase -1))))
+   (concat
+    (repeat phase 0)
+    (repeat phase 1)
+    (repeat phase 0)
+    (repeat phase -1))))
 
 (def multipliers-for-phase (memoize multipliers-for-phase-implementation))
 
-(defn get-mod [coll i cnt]
-  (nth coll (mod i cnt)))
+(defn get-mod [coll i]
+  (nth coll (mod i (count coll))))
 
-(defn multiply-by-multipliers [multipliers cnt]
+(defn multiply-by-multipliers [multipliers]
   (fn [i n]
-    (* n (get-mod multipliers (inc i) cnt))))
+    (* n (get-mod multipliers (inc i)))))
 
-(defn sum-for-multipliers [numbers multipliers cnt]
-  (as-> (multiply-by-multipliers multipliers cnt) $
-        (map-indexed $ numbers)
-        (reduce + $)
-        (Math/abs $)
-        (mod $ 10)))
+(defn sum-for-multipliers [numbers multipliers]
+  (as-> (multiply-by-multipliers multipliers) $
+    (map-indexed $ numbers)
+    (reduce + $)
+    (Math/abs $)
+    (mod $ 10)))
 
 (defn apply-phase [numbers]
   (vec
-    (map
-      #(sum-for-multipliers numbers (multipliers-for-phase (inc %)) (* 4 (inc %)))
-      (range (count numbers)))))
-
+   (map
+    #(sum-for-multipliers numbers (multipliers-for-phase (inc %)))
+    (range (count numbers)))))
 
 (defn apply-multiple-phases [numbers i]
   (if (pos? i)
     (recur (apply-phase numbers) (dec i))
     numbers))
-    
 
 (defn first-8 [numbers]
   (string/join
-    (map str (take 8 numbers))))
-
+   (map str (take 8 numbers))))
 
 (println "Part 1:"
-  (first-8
-    (apply-multiple-phases
-      (parse-file "input.txt")
-      100)))
+         (first-8
+          (apply-multiple-phases
+           (parse-file "input.txt")
+           100)))
