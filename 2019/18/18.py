4bc830007526cd49de2d5b3b71c7417712777ece
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-18 17:12:49 +0100

    2019-18, both parts, python

--- 2019/18/18.py
+++ 2019/18/18.py
@@ -4,14 +4,14 @@ KEYS = 'abcdefghijklmnopqrstuvwxyz'
 
 def parse_map(file):
     g = {}
-    current = None
+    robots = []
     with open(file) as f:
         for y, line in enumerate(f.readlines()):
             for x, char in enumerate(line.strip()):
                 g[(x, y)] = char
                 if char == '@':
-                    current = (x, y)
-    return g, current
+                    robots.append((x, y))
+    return g, tuple(robots)
 
 
 def neighbours(point):
@@ -24,59 +24,64 @@ def neighbours(point):
     ]
 
 
-def visible_keys(g, current, keys):
+def visible_keys(g, robots, keys):
     visible = set()
-    visited = set()
-    horizon = [current]
-    distance = 0
-    while horizon:
-        new_horizon = []
-        distance += 1
-        for p in horizon:
-            visited.add(p)
-            for np in neighbours(p):
-                if np in visited:
-                    continue
-                char = g.get(np, '#')
-                if char == '#':
-                    continue
-                if char in DOORS and char not in keys.upper():
-                    continue
-                if char in KEYS and char not in keys:
-                    visible.add((np, distance))
-                else:
-                    new_horizon.append(np)
-        horizon = new_horizon
+    for i, robot in enumerate(robots):
+        visited = set()
+        horizon = [robot]
+        distance = 0
+        while horizon:
+            new_horizon = []
+            distance += 1
+            for p in horizon:
+                visited.add(p)
+                for np in neighbours(p):
+                    if np in visited:
+                        continue
+                    char = g.get(np, '#')
+                    if char == '#':
+                        continue
+                    if char in DOORS and char not in keys.upper():
+                        continue
+                    if char in KEYS and char not in keys:
+                        visible.add((i, np, distance))
+                    else:
+                        new_horizon.append(np)
+            horizon = new_horizon
 
     return visible
-
-
-cache = {}
 
 
 def h(keys, current):
     return current, ''.join(sorted(keys))
 
 
-def explore(current, keys=''):
-    if h(keys, current) in cache:
-        return cache[h(keys, current)]
+def explore(robots, keys=''):
+    if h(keys, robots) in cache:
+        return cache[h(keys, robots)]
 
-    alternatives = visible_keys(g, current, keys)
+    alternatives = visible_keys(g, robots, keys)
 
     if len(alternatives) > 0:
         branches = []
-        for point, dist in alternatives:
-            s = explore(point, keys + g[point])
+        for robot, point, dist in alternatives:
+            rs = list(robots)
+            rs[robot] = point
+            rs = tuple(rs)
+            s = explore(rs, keys + g[point])
             branches.append(dist + s)
-        cache[h(keys, current)] = min(branches)
+        cache[h(keys, robots)] = min(branches)
         return min(branches)
 
     if len(alternatives) == 0:
-        cache[h(keys, current)] = 0
+        cache[h(keys, robots)] = 0
         return 0
 
 
-g, current = parse_map('input.map')
+cache = {}
+g, robots = parse_map('input.map')
+print('Part 1:', explore(robots))
 
-print('Part 1:', explore(current))
+cache = {}
+g, robots = parse_map('input2.map')
+print('Part 2:', explore(robots))
