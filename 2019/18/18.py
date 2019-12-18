a72a2c50087245b95c3059c26efb56f343c710f1
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-18 11:25:41 +0100

    2019-18, part 1, python

--- 2019/18/18.py
+++ 2019/18/18.py
@@ -1,24 +1,17 @@
-import itertools
-import copy
-
-
-DOORS = 'ABCDEFGHIJKLMNOPQRSTUVXYZ'
+DOORS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+KEYS = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def parse_map(file):
-    KEYS = 'abcdefghijklmnopqrstuvxyz'
-    grid = {}
-    keys = set()
+    g = {}
     current = None
     with open(file) as f:
         for y, line in enumerate(f.readlines()):
             for x, char in enumerate(line.strip()):
-                grid[(x, y)] = char
+                g[(x, y)] = char
                 if char == '@':
                     current = (x, y)
-                if char in KEYS:
-                    keys.add((x, y))
-    return grid, current, keys
+    return g, current
 
 
 def neighbours(point):
@@ -31,7 +24,7 @@ def neighbours(point):
     ]
 
 
-def visible_keys(grid, current, remaining_keys):
+def visible_keys(g, current, keys):
     visible = set()
     visited = set()
     horizon = [current]
@@ -44,77 +37,46 @@ def visible_keys(grid, current, remaining_keys):
             for np in neighbours(p):
                 if np in visited:
                     continue
-                char = grid.get(np, '#')
-                if char in DOORS:
+                char = g.get(np, '#')
+                if char == '#':
                     continue
-                if char != '#':
+                if char in DOORS and char not in keys.upper():
+                    continue
+                if char in KEYS and char not in keys:
+                    visible.add((np, distance))
+                else:
                     new_horizon.append(np)
-                    if np in remaining_keys:
-                        visible.add((np, distance))
         horizon = new_horizon
 
     return visible
 
 
-def pick_prefered(prefered, alternatives):
-    for p in prefered:
-        for al, d in alternatives:
-            if al == p:
-                return p, d
+cache = {}
 
 
-def explore(g, keys, current, steps=0, pick=None):
-    g = copy.deepcopy(g)
-    current = copy.deepcopy(current)
-    keys = copy.deepcopy(keys)
-
-    while keys:
-        alternatives = visible_keys(g, current, keys)
-        if len(alternatives) > 1:
-            if pick is not None:
-                point, distance = pick
-                pick = None
-            else:
-                print('creating', len(alternatives), 'branches:', [g[p] for p, _ in alternatives])
-                branches = [
-                    explore(g, keys, current, steps, copy.deepcopy(pick))
-                    for pick in alternatives
-                ]
-                return min(branches)
-
-        if len(alternatives) == 1:
-            point, distance = alternatives.pop()
-
-        current = point
-        steps += distance
-        keys.remove(current)
-        door = g[current].upper()
-
-        for p in g.keys():
-            if g[p] == door:
-                g[p] = '.'
-    return steps
+def h(keys, current):
+    return current, ''.join(sorted(keys))
 
 
-grid, current, keys = parse_map('input.map')
-print('number of keys', len(keys))
+def explore(current, keys=''):
+    if h(keys, current) in cache:
+        return cache[h(keys, current)]
 
-s = explore(grid, keys, current)
-print(s)
-'''
-steps = float('inf')
-for prefered in itertools.permutations(keys):
-    pref = list(grid.get(p) for p in prefered)
-    s = explore(
-        copy.deepcopy(grid),
-        copy.deepcopy(current),
-        copy.deepcopy(keys),
-        list(prefered)
-    )
-    if pref == ['b', 'a', 'c', 'd', 'f', 'e', 'g']:
-        print(s)
-    if s > 0:
-        steps = min(steps, s)
+    alternatives = visible_keys(g, current, keys)
 
-print('min-steps', steps)
-'''
+    if len(alternatives) > 0:
+        branches = []
+        for point, dist in alternatives:
+            s = explore(point, keys + g[point])
+            branches.append(dist + s)
+        cache[h(keys, current)] = min(branches)
+        return min(branches)
+
+    if len(alternatives) == 0:
+        cache[h(keys, current)] = 0
+        return 0
+
+
+g, current = parse_map('input.map')
+
+print('Part 1:', explore(current))
