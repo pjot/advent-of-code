ee5c64b4bfb535c4b5e085a9ab7814f28dfd5fd4
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-19 20:53:52 +0100

    2019-13, both, golang with channels

--- 2019/13/13.go
+++ 2019/13/13.go
@@ -19,13 +19,12 @@ func parseFile(fileName string) []int {
 	return program
 }
 
-
 type Mode int
 
 const (
 	Position  = 0
 	Immediate = 1
-    Relative  = 2
+	Relative  = 2
 )
 
 type OpCode int
@@ -39,37 +38,41 @@ const (
 	JumpIfFalse = 6
 	LessThan    = 7
 	Equals      = 8
-    SetBase     = 9
+	SetBase     = 9
 	Halt        = 99
 )
 
 type Computer struct {
-    memory map[int]int
-    base int
-    position int
-    output int
-    nextInput func() int
+	memory   map[int]int
+	base     int
+	position int
+	outputs  chan int
+	inputs   chan int
 }
 
-func NewComputer(program []int) Computer {
-    memory := make(map[int]int)
-    for i, val := range program {
-        memory[i] = val
-    }
-    return Computer{
-        base: 0,
-        position: 0,
-        output: 0,
-        nextInput: func () int { return 0 },
-        memory: memory,
-    }
+func NewComputer(program []int) (chan int, chan int) {
+	memory := make(map[int]int)
+	for i, val := range program {
+		memory[i] = val
+	}
+	in := make(chan int, 1)
+	out := make(chan int, 1)
+	c := Computer{
+		base:     0,
+		position: 0,
+		outputs:  out,
+		inputs:   in,
+		memory:   memory,
+	}
+	go c.run()
+	return in, out
 }
 
 func (c *Computer) g(position int) int {
-    if v, ok := c.memory[position]; ok {
-        return v
-    }
-    return 0
+	if v, ok := c.memory[position]; ok {
+		return v
+	}
+	return 0
 }
 
 func (c *Computer) read(position int, mode Mode) int {
@@ -78,8 +81,8 @@ func (c *Computer) read(position int, mode Mode) int {
 		return c.g(position)
 	case Position:
 		return c.g(c.g(position))
-    case Relative:
-        return c.g(c.base + c.g(position))
+	case Relative:
+		return c.g(c.base + c.g(position))
 	}
 	return 0
 }
@@ -90,8 +93,8 @@ func (c *Computer) write(position int, value int, mode Mode) {
 		c.memory[position] = value
 	case Position:
 		c.memory[c.g(position)] = value
-    case Relative:
-        c.memory[c.base + c.g(position)] = value
+	case Relative:
+		c.memory[c.base+c.g(position)] = value
 	}
 }
 
@@ -103,14 +106,15 @@ func parseInstruction(instruction int) (OpCode, Mode, Mode, Mode) {
 	return opCode, modeA, modeB, modeC
 }
 
-func (c *Computer) iterate() bool {
+func (c *Computer) run() {
 	for {
 		instruction := c.read(c.position, Immediate)
 		opCode, modeA, modeB, modeC := parseInstruction(instruction)
 
 		switch opCode {
 		case Halt:
-			return true
+			close(c.outputs)
+			return
 		case Add:
 			a := c.read(c.position+1, modeA)
 			b := c.read(c.position+2, modeB)
@@ -122,12 +126,11 @@ func (c *Computer) iterate() bool {
 			c.write(c.position+3, a*b, modeC)
 			c.position += 4
 		case Input:
-			c.write(c.position+1, c.nextInput(), modeA)
+			c.write(c.position+1, <-c.inputs, modeA)
 			c.position += 2
 		case Output:
-			c.output = c.read(c.position+1, modeA)
+			c.outputs <- c.read(c.position+1, modeA)
 			c.position += 2
-            return false
 		case JumpIfTrue:
 			a := c.read(c.position+1, modeA)
 			if a != 0 {
@@ -160,77 +163,72 @@ func (c *Computer) iterate() bool {
 				c.write(c.position+3, 0, modeC)
 			}
 			c.position += 4
-        case SetBase:
-            a := c.read(c.position+1, modeA)
-            c.base += a
-            c.position += 2
+		case SetBase:
+			a := c.read(c.position+1, modeA)
+			c.base += a
+			c.position += 2
 		}
 	}
 }
 
-func countBlocks (program []int) int {
-    computer := NewComputer(program)
-    outputs := []int{}
-    done := false
-    for {
-        done = computer.iterate()
-        if !done {
-            outputs = append(outputs, computer.output)
-        } else {
-            break;
-        }
-    }
-    blocks := 0
-    for i := 2; i < len(outputs); i += 3 {
-        if outputs[i] == 2 {
-            blocks++
-        }
-    }
-    return blocks
+func countBlocks(program []int) int {
+	_, out := NewComputer(program)
+	outputs := []int{}
+	for {
+		output, ok := <-out
+		if ok {
+			outputs = append(outputs, output)
+		} else {
+			break
+		}
+	}
+	blocks := 0
+	for i := 2; i < len(outputs); i += 3 {
+		if outputs[i] == 2 {
+			blocks++
+		}
+	}
+	return blocks
 }
 
 func joystick(pad int, ball int) int {
-    if pad > ball {
-        return -1
-    }
-    if ball > pad {
-        return 1
-    }
-    return 0
+	if pad > ball {
+		return -1
+	}
+	if ball > pad {
+		return 1
+	}
+	return 0
 }
 
-func playGame (program []int) int {
-    computer := NewComputer(program)
-    pad := 0
-    ball := 0
-    score := 0
+func playGame(program []int) int {
+	in, out := NewComputer(program)
 
-    for {
-        computer.nextInput = func() int { return joystick(pad, ball) }
+	pad := 0
+	ball := 0
+	score := 0
 
-        computer.iterate()
-        x := computer.output
+	for {
+		x, ok := <-out
+		if !ok {
+			return score
+		}
+		y := <-out
+		tile := <-out
 
-        computer.iterate()
-        y := computer.output
+		if x == -1 && y == 0 {
+			score = tile
+		}
 
-        done := computer.iterate()
-        tile := computer.output
+		switch tile {
+		case 4:
+			ball = x
+			in <- joystick(pad, ball)
+		case 3:
+			pad = x
+		}
 
-        switch tile {
-        case 4:
-            ball = x
-        case 3:
-            pad = x
-        }
-
-        if x == -1 && y == 0 {
-            score = tile
-        }
-        if done {
-            return score
-        }
-    }
+	}
 }
 
 func main() {
@@ -238,6 +236,6 @@ func main() {
 
 	fmt.Println("Part 1:", countBlocks(program))
 
-    program[0] = 2
-    fmt.Println("Part 2:", playGame(program))
+	program[0] = 2
+	fmt.Println("Part 2:", playGame(program))
 }
