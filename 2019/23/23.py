7e36c71232564df0df43fbe10f7cba7c88179645
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-23 14:32:37 +0100

    refactor 23 to handle both parts

--- 2019/23/23.py
+++ 2019/23/23.py
@@ -1,84 +1,40 @@
-from collections import defaultdict
 from intcode import parse_file, Computer
 
 
-def run_one(queues, network):
+def run():
+    network = {
+        i: Computer(parse_file('input.intcode'), [i])
+        for i in range(50)
+    }
+
+    nat = None
+    first_y = None
+
     while True:
-        for i, c in network.items():
-            if len(queues[i]) > 0:
-                c.inputs.append(queues[i].pop(0))
-            else:
-                c.inputs.append(-1)
+        empty_outputs = all([c.output is None for c in network.values()])
 
-            c.iterate()
-            addr = c.output
-            if addr is None:
-                continue
-            c.iterate()
-            x = c.output
-            c.iterate()
-            y = c.output
+        if empty_outputs and nat:
+            network[0].inputs += list(nat)
 
-            if 0 <= addr < 50:
-                queues[addr].append(x)
-                queues[addr].append(y)
+        for _, c in network.items():
+            c.output = None
+
+            addr = c.iterate_once()
+            x = c.iterate_once()
+            y = c.iterate_once()
 
             if addr == 255:
-                return y
+                if first_y is None:
+                    first_y = y
+                if nat and nat[1] == y:
+                    return first_y, y
+                nat = x, y
+            elif addr is None:
+                c.inputs.append(-1)
+            else:
+                network[addr].inputs += [x, y]
 
 
-def run_two(network, nat):
-    last_nat = [None, None]
-    while True:
-        empty_inputs = all([len(c.inputs) == 0 for c in network.values()])
-        empty_outputs = all([c.output == None for c in network.values()])
-        empty_nat = nat == [None, None]
-        if empty_outputs and not empty_nat:
-            network[0].inputs.append(nat[0])
-            network[0].inputs.append(nat[1])
-            if last_nat[1] == nat[1]:
-                return nat[1]
-            last_nat = nat
-        for i, c in network.items():
-            c.output = None
-            c.iterate()
-            addr = c.output
-            c.iterate()
-            x = c.output
-            c.iterate()
-            y = c.output
-
-            if addr is None:
-                if len(c.inputs) == 0:
-                    c.inputs.append(-1)
-            elif addr == 255:
-                nat = [x, y]
-            elif addr < 50 and addr >= 0:
-                network[addr].inputs.append(x)
-                network[addr].inputs.append(y)
-
-
-def part_two():
-    network = {}
-    for i in range(50):
-        program = parse_file('input.intcode')
-        c = Computer(program, [i])
-        network[i] = c
-
-    return run_two(network, [None, None])
-
-
-def part_one():
-    queues = {}
-    network = {}
-    for i in range(50):
-        program = parse_file('input.intcode')
-        c = Computer(program, [i])
-        queues[i] = []
-        network[i] = c
-
-    return run_one(queues, network)
-
-
-print("Part 1:", part_one())
-print("Part 2:", part_two())
+one, two = run()
+print("Part 1:", one)
+print("Part 2:", two)
