004d196c80562fc98e3c5666b8767a68349cec7f
Peter Bergström <peter.bergstrom@trioptima.com>
2020-12-02 07:38:52 +0100

    2020-02, both, python

--- 2020/02/02.py
+++ 2020/02/02.py
@@ -1,17 +1,20 @@
-passwords = []
-with open("input.txt") as f:
-    for line in f.readlines():
-        policy, letter, pw = line.strip().split(' ')
-        a, b = policy.split('-')
-        letter = letter[0]
-        passwords.append((int(a), int(b), letter, pw))
+import re
+
+PATTERN = re.compile(r'(\d+)-(\d+) ([a-z]): ([a-z]+)')
+
+def parse(line):
+    a, b, letter, pw = re.match(PATTERN, line).groups()
+    return int(a), int(b), letter, pw
+
+with open('input.txt') as f:
+    passwords = [parse(line) for line in f.readlines()]
 
 one = 0
 two = 0
 for a, b, letter, pw in passwords:
     if a <= pw.count(letter) <= b:
         one += 1
-    if (pw[a-1] == letter) ^ (pw[b-1] == letter):
+    if (pw[a - 1] == letter) ^ (pw[b - 1] == letter):
         two += 1
 
 print('Part 1:', one)
