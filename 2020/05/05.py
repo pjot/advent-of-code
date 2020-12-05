3bdc3c64da415c68cf5c03e00b353a3f89b0d82f
Peter Bergström <peter.bergstrom@trioptima.com>
2020-12-05 07:27:00 +0100

    2020-05, python

--- 2020/05/05.py
+++ 2020/05/05.py
@@ -1,20 +1,18 @@
 with open('input.txt') as f:
     specs = [line.strip() for line in f.readlines()]
 
-highest = 0
 seats = set()
 for spec in specs:
-    row = int(spec[:7].replace('F', '0').replace('B', '1'), 2)
-    col = int(spec[7:].replace('L', '0').replace('R', '1'), 2)
-    seat_id = row * 8 + col
+    seat_id = int(
+        spec.replace('F', '0').replace('B', '1')
+            .replace('L', '0').replace('R', '1'),
+        2
+    )
     seats.add(seat_id)
-    highest = max(seat_id, highest)
 
-for i in range(highest):
-    if i in seats:
-        continue
-    if i-1 in seats and i+1 in seats:
+for i in range(min(seats), max(seats)):
+    if i not in seats:
         my_seat = i
 
-print("Part 1:", highest)
+print("Part 1:", max(seats))
 print("Part 2:", my_seat)
