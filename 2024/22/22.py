0801eea658fbe043558fe3465f1bb3041af179b4
Peter Bergström <peter.bergstrom@trioptima.com>
2024-12-22 11:36:43 +0100

    2024-22, refactored

--- 2024/22/22.py
+++ 2024/22/22.py
@@ -1,7 +1,4 @@
-import itertools
-
-Change = tuple[int, ...]
-Changes = list[dict[Change, int]]
+from collections import defaultdict
 
 def parse(file: str) -> list[int]:
     secrets = []
@@ -12,54 +9,39 @@ def parse(file: str) -> list[int]:
                 secrets.append(int(line))
     return secrets
 
-def generate(n: int) -> int:
-    b = n * 64
-    n ^= b
-    n %= 16777216
-
-    c = n // 32
-    n ^= c
-    n %= 16777216
-
-    d = n * 2048
-    n ^= d
-    n %= 16777216
-    return n
+def next_number(n: int) -> int:
+    p = 2 ** 24
+    n = n ^ (n * 2 ** 6) % p
+    n = n ^ (n // 2 ** 5) % p
+    return n ^ (n * 2 ** 11) % p
 
 def last_digit(n: int) -> int:
     return int(str(n)[-1])
 
-def changes(secrets: list[int]) -> tuple[Changes, int]:
-    changes = []
-    one = 0
-    for s in secrets:
+changes = defaultdict(int)
+one = 0
+with open("input") as f:
+    for line in f.readlines():
+        seen = set()
+
+        line = line.strip()
+        if not line:
+            continue
+
+        s = int(line)
         values = [s]
-        change_value: dict[Change, int] = {}
         for i in range(2000):
-            s = generate(s)
+            s = next_number(s)
             values.append(last_digit(s))
+
             if len(values) > 5:
                 a, b, c, d, e = values[-5:]
                 change = (b - a, c - b, d - c, e - d)
-                if change_value.get(change) is None:
-                    change_value[change] = e
+                if not change in seen:
+                    changes[change] += e
+                seen.add(change)
+
         one += s
-        changes.append(change_value)
-    return changes, one
-
-def bananas(deltas: Changes) -> int:
-    best = 0
-    attempts: list[Change] = []
-    for attempt in itertools.product(range(-3, 4), repeat=4):
-        total = sum(
-            d.get(attempt, 0)
-            for d in deltas
-        )
-        best = max(best, total)
-    return best
-
-secrets = parse("input")
-deltas, one = changes(secrets)
 
 print("Part 1:", one)
-print("Part 2:", bananas(deltas))
+print("Part 2:", max(changes.values()))
