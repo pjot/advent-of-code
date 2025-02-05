c8328c7dd5ae8343e20beef95bbbf023f429ffac
Peter Bergström <peter.bergstrom@trioptima.com>
2025-02-05 12:56:52 +0100

    2024-24, refactored

--- 2024/24/24.py
+++ 2024/24/24.py
@@ -1,5 +1,4 @@
 import itertools
-from collections import defaultdict
 from dataclasses import dataclass
 
 @dataclass
@@ -17,8 +16,10 @@ class Gate:
         if self.operation == "AND":
             return a and b
 
+    def parents(self, gates):
+        return [gates[self.a], gates[self.b]]
 
-wires = {}
+values = {}
 gates = {}
 
 with open("input") as f:
@@ -28,38 +29,37 @@ with open("input") as f:
         a, operation, b, _, wire = line.strip().split()
 
         gates[wire] = Gate(a, b, operation, wire)
-        wires[a] = None
-        wires[b] = None
-        wires[wire] = None
+        values[a] = None
+        values[b] = None
+        values[wire] = None
 
     for line in w.splitlines():
         wire, value = line.strip().split(": ")
-        wires[wire] = value == "1"
+        values[wire] = value == "1"
 
-
-def solve(wires, gates):
+def solve(values, gates):
     def number(v):
-        my_wires = [w for w in wires.keys() if w.startswith(v)]
+        my_values = [w for w in values.keys() if w.startswith(v)]
         word = ""
-        for wire in sorted(my_wires):
-            c = "1" if wires[wire] else "0"
+        for wire in sorted(my_values):
+            c = "1" if values[wire] else "0"
             word = c + word
         return int(word, 2)
 
-    none_wires = {wire for wire, val in wires.items() if val is None}
+    none_values = {
+        wire for wire, val in values.items() if val is None
+    }
 
-    while none_wires:
+    while none_values:
         done = set()
-        for wire in none_wires:
-            g = gates[wire]
-            a, b = wires.get(g.a), wires.get(g.b)
+        for wire in none_values:
+            gate = gates[wire]
+            a, b = values.get(gate.a), values.get(gate.b)
             if a is not None and b is not None:
-                wires[wire] = g.operate(a, b)
+                values[wire] = gate.operate(a, b)
                 done.add(wire)
 
-        none_wires -= done
-        if len(done) == 0:
-            return 1, 1, 2
+        none_values -= done
 
     return number("x"), number("y"), number("z")
 
@@ -77,15 +77,12 @@ def two():
         if w.target == "z00":
             continue
 
-        gate_a = gates[w.a]
-        gate_b = gates[w.b]
-
-        for gate in [gate_a, gate_b]:
+        for gate in w.parents(gates):
             if gate.operation == "XOR" and gate.a[0] not in "xy":
                 bad.append(gate.target)
 
             if gate.operation == "OR":
-                for sub_gate in [gates[gate.a], gates[gate.b]]:
+                for sub_gate in gate.parents(gates):
                     if sub_gate.operation != "AND":
                         bad.append(sub_gate.target)
 
@@ -96,7 +93,7 @@ def two():
 
     return ",".join((sorted(bad)))
 
-x, y, z = solve(wires, gates)
+x, y, z = solve(values, gates)
 print("Part 1:", z)
 print("Part 2:", two())
 
